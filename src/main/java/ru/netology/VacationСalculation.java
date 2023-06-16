9ed11764617c8ceeb2ee77e1c1525b54708e282e
Maryia <tsapkova_mariya@bk.ru>
2023-06-16 14:36:24 +0500

    #4

--- src/main/java/ru/netology/VacationСalculation.java
+++ src/main/java/ru/netology/VacationСalculation.java
@@ -1,21 +1,21 @@
 
 package ru.netology;
 
-public class vacationСalculation {
-    public int calc(int income, int expense, int money) {
+public class VacationСalculation {
+    public int calc(int income, int expense, int threshold) {
 
         // income - доход от работы
         // expense - месячные обязательные траты
         // threshold - нужно денег для отпуска
         // money - денег на счету
         int count = 0;
-        // int threshold = 20000;
+
         for (int month = 0; month < 12; month++) {
-            if (expense >= money) { // можем ли отдыхать?
-                money = money + income - expense;
+            if (expense >= threshold) { // можем ли отдыхать?
+                threshold = threshold + income - expense;
             } else {
                 count++; // увеличиваем счётчик месяцев отдыха
-                money = (money - expense) / 3;
+                threshold = (threshold - expense) / 3;
             }
         }
         return count;
--- src/test/java/ru/netology/VacationCalculationTest.java
+++ src/test/java/ru/netology/VacationCalculationTest.java
@@ -2,10 +2,9 @@ package ru.netology;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
 
-public class vacationCalculationTest {
+public class VacationCalculationTest {
 
     @ParameterizedTest
     @CsvSource({
@@ -13,12 +12,12 @@ public class vacationCalculationTest {
             "60000,5000,0,8"
     })
 // @CsvFileSource(files = "src/test/resources/Dannue1.csv")
-    public void numberOfMonthSatLowCost(int income, int expense, int money, int expected) {
-        vacationСalculation service = new vacationСalculation();
+    public void numberOfMonthSatLowCost(int income, int expense, int threshold, int expected) {
+        VacationСalculation service = new VacationСalculation();
 
 
         // вызываем целевой метод:
-        int actual = service.calc(income, expense, money);
+        int actual = service.calc(income, expense, threshold);
 
         // производим проверку (сравниваем ожидаемый и фактический):
         Assertions.assertEquals(expected, actual);
